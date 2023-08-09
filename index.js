b454384735db99c45dcbb1b219109c067c5e834c
Abhishek Kumar <68455173+ak10rn@users.noreply.github.com>
2023-08-09 15:06:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ const app = express();
 const PORT = process.env.NODE_CLIENT_PORT || 3001;
 const BASEURL = process.env.NODE_SERVER_BASEURL;
 app.get("/", (req, res) => {
+  try{
   const data = fetch(BASEURL).then(res => res.json());
-  res.send("Hello There!!");
+  res.send(data);
+  } catch (...) {
+    res.send("An Error Occurred! :(");
+    
+  }
 });
 
 app.listen(PORT, () => {
