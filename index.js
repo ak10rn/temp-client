877baf850910d0ca8d08a68ac3c66b5a429e4921
Abhishek Kumar <68455173+ak10rn@users.noreply.github.com>
2023-08-09 17:19:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const express = require("express");
+const axios = require("axios");
+
 const app = express();
 
 const PORT = process.env.NODE_CLIENT_PORT || 3001;
 const BASEURL = process.env.NODE_SERVER_BASEURL;
-app.get("/", (req, res) => {
-  try{
-    const data = fetch(BASEURL).then(res => res.json());
-    const response = PORT + BASEURL + data;
-    res.send(response);
-  } catch (err) {
-    res.send("An Error Occurred! :(");
-  }
+
+async function getData() {
+  const { data } = await axios.get(BASEURL);
+  return data;
+
+app.get("/", async (req, res) => {
+  res.send(await getData());
 });
 
 app.listen(PORT, () => {
